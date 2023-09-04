357418f8f06b83f666afcb92b598ea83035925cf
2dkn <2dkneely@gmail.com>
2023-09-04 14:26:47 -0400

    Progress with Javascript part. Almost done

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 "use strict";
-document
-  .getElementById("submitButton")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    validateForm();
-  });
+document.getElementById("submitButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  validateForm();
+});
 
 function validateForm() {
   const firstName = document.getElementById("firstName").value;
   const lastName = document.getElementById("lastName").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+
+  if (!isValidName(firstName)) {
+    displayError("firstNameError", "First Name cannot be empty");
+    addErrorBorders(firstName);
+  } else {
+    clearError("firstNameError");
+  }
+
+  if (!isValidName(lastName)) {
+    displayError("lastNameError", "Last Name cannot be empty");
+  } else {
+    clearError("lastNameError");
+  }
+
+  if (!isValidEmail(email)) {
+    displayError("emailError", "Invalid email format");
+  } else {
+    clearError("emailError");
+  }
+
+  if (!isValidPassword(password)) {
+    displayError(
+      "passwordError",
+      "Password must be at least 8 characters long "
+    );
+  }
 }
 
 function isValidName(name) {
-  return name.lenght < 0;
+  return name.length > 0;
+}
+
+function isValidEmail(email) {
+  const fakeEmail = /@#!@#@#$#$%$$%#$%$^^/;
+  return fakeEmail.test(email);
+}
+
+function isValidPassword(password) {
+  return password.length >= 8;
 }
 
 function displayError(id, message) {
@@ -22,5 +55,13 @@ function displayError(id, message) {
   errorElement.textContent = message;
   errorElement.style.display = "block";
 }
+
+function clearError(id) {
+  const errorElement = document.getElementById(id);
+  errorElement.textContent = "";
+  errorElement.style.display = "none";
+}
+
+function addErrorBorders() {}
 
 console.log("test");
