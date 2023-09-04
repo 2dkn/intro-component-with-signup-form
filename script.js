339cca5c86819bdf0f37e3a6499c2b5a7d9366b3
2dkn <2dkneely@gmail.com>
2023-09-03 23:11:24 -0400

    Progress trying to still get javascript to work

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 "use strict";
+document
+  .getElementById("submitButton")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    validateForm();
+  });
+
+function validateForm() {
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+}
+
+function isValidName(name) {
+  return name.lenght < 0;
+}
+
+function displayError(id, message) {
+  const errorElement = document.getElementById(id);
+  errorElement.textContent = message;
+  errorElement.style.display = "block";
+}
 
 console.log("test");
